764a845faaad76b4c2ff5b5e183d85ca24bab505
Richard Chiu <richardchiu@Richards-MacBook-Air.local>
2023-08-29 18:04:26 -0400

    listing card add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,29 @@
 import Image from "next/image";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import getListings from "./actions/getListings";
+import ListingCard from "./components/listings/ListingCard";
+import getCurrentUser from "./actions/getCurrentUser";
 
-export default function Home() {
-  return <div className="text-3xl text-rose-500">Hello</div>;
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  console.log(listings);
+  if (listings.length === 0) {
+    return <EmptyState showReset />;
+  }
+  return (
+    <Container>
+      <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        {listings.map((listing: any) => (
+          <ListingCard
+            key={listing.id}
+            data={listing}
+            currentUser={currentUser}
+          />
+        ))}
+      </div>
+    </Container>
+  );
 }
