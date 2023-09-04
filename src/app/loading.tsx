f8fb28e8d6361bd7e1100b77086f52d3dda9f250
Richard Chiu <richardchiu@Richards-MacBook-Air.local>
2023-09-04 14:24:16 -0400

    loader add

---
+++ src/app/loading.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Loader from "./components/Loader";
+
+const Loading = () => {
+  return (
+    <div className="h-[80vh] flex items-center justify-center">
+      <Loader />
+    </div>
+  );
+};
+
+export default Loading;
